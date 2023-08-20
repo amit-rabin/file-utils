fa25ae03e955e20fe7b16b7f0836721bfeba872f
amit-rabin <62716351+amit-rabin@users.noreply.github.com>
2023-08-20 23:55:59 +0300

    tested.

--- run.py
+++ run.py
@@ -6,5 +6,3 @@ os_file_handler = OSFileHandler(
     base_route=config["base_route"],
     os_file_handling_action_to_encoding_mapping=config["os_file_handling_action_to_encoding_mapping"],
     action_to_error_message_mapping=config["action_to_error_message_mapping"])
-
-os_file_handler.create_file_and_write(path="tests", filename="test1", file_type="text", text="amitos is the best")
--- src/config/config_loader.py
+++ src/config/config_loader.py
@@ -1,8 +1,9 @@
 import yaml
 
 config = None
+route_to_config = "C:/dev/active/file_utils/src/config/config.yml"
 
-with open("C:/dev/active/file_utils/src/config/config.yml", "r") as stream:
+with open(route_to_config, "r") as stream:
     try:
         config = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
